07b83218bb0aa788bdad6a9e84cd2f9700b2a0a0
nitikeshs58 <nitikeshs58@gmail.com>
2020-03-22 11:54:51 +0530

    Added: total amount win or call after 20 days.

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -8,15 +8,25 @@ BET=1
 stake=100
 win=0
 lost=0
-
-while(( stake<150 && stake>50 ))
+day=0
+totalWin=0
+totalLost=0
+while(( $day<20 ))
 do
-	gamble=$((RANDOM%2))
-	if [[ $gamble -eq 1 ]]
-	then
-		stake=$((stake+1))
-	else
-		stake=$((stake-1))
-	fi
+	while(( $stake<150 && $stake>50 ))
+	do
+		gamble=$((RANDOM%2))
+		if [[ $gamble -eq 1 ]]
+		then
+			stake=$((stake+BET))
+			totalWin=$((totalWin+BET))
+		else
+			stake=$((stake-BET))
+			totalLost=$((totalLost+1))
+		fi
+	done
+	day+=1
 done
 echo $stake
+echo "TotalWin: $totalWin  TotalLost: $totalLost"
+
