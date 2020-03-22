b96914b628de35323088635b196b48dffae7dd38
nitikeshs58 <nitikeshs58@gmail.com>
2020-03-22 11:03:45 +0530

    Added:Gambler makes 1$ bet.

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -6,4 +6,17 @@ BET=1
 
 #Variables
 stake=100
+win=0
+lost=0
 
+for (( game=0; game<1; game++ ))
+do
+	gamble=$((RANDOM%2))
+	if [[ $gamble -eq 1 ]]
+	then
+		stake=$((stake+1))
+	else
+		stake=$((stake-1))
+	fi
+done
+echo $stake
